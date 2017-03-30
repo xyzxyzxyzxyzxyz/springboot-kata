d13c3fada3449addebfc38bff215b8279b39f461
XYZ <idm.caz@mailinator.com>
2017-03-30 15:38:41 +0200

    Initial commit

---
+++ src/main/java/com/tdd/katas/springboot/SpringBootKataApplication.java
@@ -0,0 +1,13 @@
+package com.tdd.katas.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tdd/katas/springboot/SpringBootKataApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tdd.katas.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootKataApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
