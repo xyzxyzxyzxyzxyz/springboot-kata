9cb38baf9b8aa0f5e9823b722b10894b335feadf
XYZ <idm.caz@mailinator.com>
2017-04-05 16:56:14 +0200

    New test: UserRepository#findAllByLoginPasswordEmail() Should return all matching users, and should not return any of the unmatching users. Failing test.

--- src/main/java/com/tdd/katas/springboot/repositories/UserRepository.java
+++ src/main/java/com/tdd/katas/springboot/repositories/UserRepository.java
@@ -3,5 +3,10 @@ package com.tdd.katas.springboot.repositories;
 import com.tdd.katas.springboot.model.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface UserRepository extends CrudRepository<User,Long>{
+
+    List<User> findAllByLoginPasswordEmail(User userFilter);
+
 }
